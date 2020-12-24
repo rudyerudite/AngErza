5045d8e5f5294c8e724202d95aaeb5c8c19e30b5
rudyerudite03 <dixitshweta22@gmail.com>
2020-12-24 14:17:16 +0530

    adding mitigation checker src:Zeratool

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. [https://docs.angr.io/core-concepts/toplevel]
 3. [https://docs.google.com/document/d/1lVNkMIpvbN4d1yHUDGyeMagvCfFYF2dZk_2aPGq29U0/edit?usp=sharing]
 4. [https://docs.google.com/document/d/1PaHscqISceMk-FB37-jqTke7_q2GPxYfSt2-x7ngVQE/edit?usp=sharing]
+5. [https://0x00sec.org/t/autobof-a-journey-into-automation-exploit-development-and-buffer-overflows/13415]
--- finder.py
+++ finder.py
@@ -1,8 +1,33 @@
+from pwn import *
 import claripy
 import angr
 
 proj = angr.Project("bug",auto_load_libs=False)
+binary = ELF("bug")
 
+properties = {}
 gets = proj.loader.find_symbol('gets')
-
 print(gets)
+
+def findmitigation():
+	# reference: https://github.com/ChrisTheCoolHut/Zeratool/blob/master/lib/protectionDetector.py
+	# other trials
+	'''
+	if(proj.loader.main_object.execstack== True):
+		print("NX is enabled")
+	elif(proj.loader.main_object.pic == True):
+		print("PIE is enabled")
+	'''
+	properties['aslr'] = binary.aslr
+    properties['arch'] = binary.arch
+    properties['canary'] = binary.canary
+    properties['got'] = binary.got
+    properties['nx'] = binary.nx
+    properties['pie'] = binary.pie
+    properties['plt'] = binary.plt
+    properties['relro'] = binary.relro
+
+    return properties
+
+
+
