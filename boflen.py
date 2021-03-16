d32c449419682fb27f58b72546fc75ba45cbc045
rudyerudite03 <dixitshweta22@gmail.com>
2021-03-16 21:04:09 +0530

    [+] r2 script: symbolically declaring varaibles where i/p is taken

--- boflen.py
+++ boflen.py
@@ -8,6 +8,7 @@ input_functions = ["read","fgets","gets","__isoc99_scanf"]
 binary =  sys.argv[1]
 function = {}
 overflow = 0
+size_array = []
 size = 0
 def findfunctions(name):
 # reference: https://docs.angr.io/built-in-analyses/identifier
@@ -18,42 +19,65 @@ def findfunctions(name):
 	for fninfo in id_.func_info:
 		print(hex(fninfo.addr), fninfo.name)
 		function[fninfo.name] = fninfo.addr
-findfunctions(binary)
-p = process(binary)
-r = r2pipe.open(binary,flags = ["-d"])
 
-stdin_fns = list((set(function.keys())).intersection(input_functions))
-print(stdin_fns)
-for fn in stdin_fns:
-	log.info("Continuing until {}".format(hex(function[fn])))
-	r.cmd('db {}'.format(hex(function[fn])))
-	r.cmd('dc')
-	rdi = r.cmd('dr rdi')
-	rsi = r.cmd('dr rsi')
-	rdx = r.cmd('dr rdx')
-	rbp = r.cmd('dr rbp')
-	print(rdi,rsi,rdx,rbp)
-	if(fn=="fgets" or fn=="read"):
-		if(fn == "fgets"):
-			size = int(rbp,16)-int(rdi,16)
-			inpsize = int(rsi,16)
-		elif(fn == "read"):
-			size = int(rbp,16)-int(rsi,16)
-			inpsize = int(rdx,16)
-		if(size < inpsize ):
-			overflow = inpsize-size
-			lof.info("BOF found size: {}".format(overflow))
-	elif(fn=="gets"):
-		overflow = 256
-		log.info("[+] gets found")
-	elif(fn=="__isoc99_scanf"):
-		m = r.cmd('psz @rdi')
-		# doesn't check for %ms type args where m > size of the buf
-		if("%s" in m):
+def stdin_fn():
+	local = []
+	findfunctions(binary)
+	p = process(binary)
+	r = r2pipe.open(binary,flags = ["-d"])
+	i = 0
+	stdin_fns = list((set(function.keys())).intersection(input_functions))
+	print(stdin_fns)
+	for fn in stdin_fns:
+		log.info("Continuing until {}".format(hex(function[fn])))
+		r.cmd('db {}'.format(hex(function[fn])))
+		r.cmd('dc')
+		rdi = r.cmd('dr rdi')
+		rsi = r.cmd('dr rsi')
+		rdx = r.cmd('dr rdx')
+		rbp = r.cmd('dr rbp')
+		print(rdi,rsi,rdx,rbp)
+		if(fn=="fgets" or fn=="read"):
+			if(fn == "fgets"):
+				size = int(rbp,16)-int(rdi,16)
+				inpsize = int(rsi,16)
+			elif(fn == "read"):
+				size = int(rbp,16)-int(rsi,16)
+				inpsize = int(rdx,16)
+			if(size < inpsize ):
+				overflow = inpsize-size
+				i+=1
+				log.info("BOF found size: {}".format(overflow))
+			else:
+				overflow = 0
+		elif(fn=="gets"):
 			overflow = 256
-			log.info("[+] overflow in scanf found")
+			log.info("[+] gets found")
+		elif(fn=="__isoc99_scanf"):
+			m = r.cmd('psz @rdi')
+		# doesn't check for %ms type args where m > size of the buf
+			if("%s" in m):
+				overflow = 256
+				log.info("[+] overflow in scanf found")
+	
+		if(overflow!=0):
+			local.append(claripy.BVS('local'+str(i),inpsize*8))
+			size_array.append(overflow)
+			i+=1
+		overflow = 0
 
-return overflow 
+	return local,size_array
+#how to add a check to find if there's an internal buffer overflow; where we can overflow one buffer and then corrupt the nearby variables
+#can rbp corruption cause any issue?
+#using radare to see which all local variables are declared and then finding their size
+#r.cmd('aa')
+#print(r.cmd('afvd'))
+
+findfunctions(binary)
+stdin_fn()
+
+	
+
 
 
 
