f7e92a37afc60d48e40cb5bc8f330631b97a70ad
rudyerudite03 <dixitshweta22@gmail.com>
2021-05-26 11:40:01 +0530

    [+] modifications to main

--- main.py
+++ main.py
@@ -4,33 +4,44 @@ from lib import file_properties
 from lib import boflen
 from lib import exploitgen
 from lib import shellcode_detect
-<<<<<<< HEAD
 from lib import format_string
-=======
->>>>>>> d079517d21cdd80064bf1580dd4939e355344833
 
 binary = sys.argv[1]
 
+#loading the project for analysis
 proj = angr.Project(binary,auto_load_libs=False)
 state = proj.factory.entry_state(stdin=angr.SimFile)
 simgr = proj.factory.simulation_manager(state,save_unconstrained=True)
-#simgr.stashes['bof'] = []
-properties = file_properties.file_parser(binary)
-format_string = format_string.format_string_detect(binary,function)
+
+#finding file properties
+file_properties = file_properties.file_parser(binary)
+print(file_properties)
+properties = boflen.findmitigation(binary,file_properties)
+#finding buffer overflow property
 local, size_array, size = boflen.stdin_fn(binary)
-print(size_array)
-if(size_array == []):
+
+if(size_array == [] and [properties['nx']] != [False]):
     print("\n>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<\n")
     print("NO OVERFLOW DETECTED")
     print("\n>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<\n")
-    #format_detect.check_fmt(binary)
-    exit(0)
-properties = boflen.findmitigation(binary,properties)
+    print("\n CHECKING FOR FORMAT STRING BUG...\n")
+    function = boflen.findfunctions(binary)
+    format_string = format_string.format_string_detect(binary,function)
 
-#win_addr = boflen.find_win(simgr)
-
-if(properties['nx'] == False):
-    shellcode_detect.give_shell(size)
+    if(format_string == True):
+        print("\n FORMAT STRING BUG DETECTED\n")
+    else:
+        print("\n...FORMAT STRING BUG NOT DETECTED")
 else:
-    exploitgen.find_win_rop(size)
+    s = 0
+    k = ""
+    if([properties['nx']] == [False]):
+        print("\n CHECKING FOR SHELLCODE PAYLOAD...\n")
+        s = shellcode_detect.give_shell(size)
+    if(s == 0 and not(size_array == [])):
+        print("\n CHECKING FOR ROPCHAIN...\n")
+        k = exploitgen.find_win_rop(size)
+    if( s==0 and k == "" and not(size_array == [])):
+        print("\n CHECKING FOR SYSCALL ROPCHAIN ...\n")
+        exploitgen.find_rop(size)
 
